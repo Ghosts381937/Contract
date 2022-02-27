d712e9808382e2b70f2b06403d2e2ec5fd267e57
Ghosts381937 <she.xing88@gmail.com>
2022-02-27 12:19:22 +0800

    Increment initPlayer function to initialize Player sturcture

--- contracts/Game/U_game.sol
+++ contracts/Game/U_game.sol
@@ -1,14 +1,19 @@
+
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.4.0 <0.9.0;
 contract U_game{
     struct Player{
         uint8 player_site;
+        bool isInited;
     }
     mapping (address => Player) private player;
 
-    function readSite() external returns(uint8){
-        if(player[msg.sender].player_site == 0)
-            player[msg.sender].player_site = 1;
+    function initPlayer() external {
+        require(player[msg.sender].isInited == false, "You had been inited!");
+        player[msg.sender].player_site = 1;
+    }
+
+    function readSite() external view returns(uint8){
         return player[msg.sender].player_site;
     }
 
