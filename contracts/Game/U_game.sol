fbf951d699f83f126df30ecddf2957f8425d2ed4
D0845073 <D0845073@o365.fcu.edu.tw>
2022-03-08 17:41:24 +0800

    H_Z contract

--- contracts/Game/U_game.sol
+++ contracts/Game/U_game.sol
@@ -5,10 +5,11 @@ contract U_game{
         uint quantityOfEnemy;
     }
     struct Player{
-        uint8 player_site;
+        uint256 player_site;
         bool isInited;
     }
     Map[10] public map;
+    mapping (address => Player) private player;
     constructor(){
         map[2].quantityOfEnemy = 2;
         map[3].quantityOfEnemy = 3;
@@ -19,29 +20,29 @@ contract U_game{
         map[8].quantityOfEnemy = 8;
         map[9].quantityOfEnemy = 9;
     }
-    mapping (address => Player) private player;
-
     function initPlayer() external {
         require(player[msg.sender].isInited == false, "You had been inited!");
         player[msg.sender].player_site = 1;
     }
-
-    function readSite(address _player) external view returns(uint8){
+    function readSite(address _player) external view returns(uint256){
         return player[_player].player_site;
     }
-
-    function moveSite(uint8 _NextSite) external {
+    function moveSite(uint256 _NextSite) external {
         require(_NextSite >= 2 && _NextSite <= 10, "Your input nextsite is unreasonable!");
         player[msg.sender].player_site = _NextSite;
     }
-
-    function killEnemy(uint _index, uint _quantityOfEnemy) external{
+    function killEnemy(uint256 _index, uint256 _quantityOfEnemy) external{
         require(_index >= 2 && _index <= 9, "Your input index is unreasonable!");
         require(_quantityOfEnemy >= 1, "Your input quantityOfEnemy is unreasonable!");
         require(_quantityOfEnemy <= map[_index].quantityOfEnemy, "Your input quantityOfEnemy is out of range!");
         map[_index].quantityOfEnemy -= _quantityOfEnemy;
     }
-    function random_value(uint _seed) internal view returns(uint){
+    function addEnemy(uint256 _index, uint256 _quantityOfEnemy) external{
+        require(_index >= 2 && _index <= 9, "Your input index is unreasonable!");
+        require(_quantityOfEnemy >= 1, "Your input quantityOfEnemy is unreasonable!");
+        map[_index].quantityOfEnemy += _quantityOfEnemy;
+    }
+    function random_value(uint256 _seed) internal view returns(uint){
         uint seed_temp =  uint(keccak256(abi.encodePacked(blockhash(block.number - 1), _seed)));
         return seed_temp;
     }
