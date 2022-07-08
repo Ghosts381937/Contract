ed1eceea6e67e3ba0a8064ad03c2e72ab718e18b
Ghosts381937 <she.xing88@gmail.com>
2022-07-08 16:22:55 +0800

    Automatic deploy the contract, and add some unit tests.

--- migrations/1_migration.js
+++ migrations/1_migration.js
@@ -1,6 +1,14 @@
-var TFToken = artifacts.require("TFToken");
+const CURRENCY = artifacts.require("Currency");
+const NFT = artifacts.require("Nft");
+const MARKET = artifacts.require("Market");
 
-module.exports = function(deployer) {
+module.exports = async function(deployer, nil, accounts) {
   // deployment steps
-  deployer.deploy(TFToken, "1000");
-};
+  await deployer.deploy(CURRENCY)
+  await deployer.deploy(NFT)
+  await deployer.deploy(MARKET, CURRENCY.address, NFT.address)
+
+  const nft = await NFT.deployed();
+  await nft.setSpecificContract(MARKET.address, {from:accounts[0]})
+  
+}
---
+++ test/UnitTest.js
@@ -0,0 +1,26 @@
+const CURRENCY = artifacts.require("Currency");
+const NFT = artifacts.require("Nft");
+const MARKET = artifacts.require("Market");
+
+contract("CURRENCY", (accounts) => {
+  it("should put 10^22 TFToken in the first account", async () => {
+    const currency = await CURRENCY.deployed();
+    const balance = await currency.balanceOf.call(accounts[0]);
+
+    assert.equal(balance.valueOf(), web3.utils.toWei('10000', 'ether'), "10000 wasn't in the first account");
+  });
+});
+contract("NFT", (accounts) => {
+  it("should be 'Test' in the [name]", async () => {
+    const nft = await NFT.deployed();
+    const name = await nft.name.call();
+
+    assert.equal(name, 'Test', "'Test' wasn't in the [name]" + );
+  });
+  it("should be MARKET.address in the [specificContract]", async () => {
+    const nft = await NFT.deployed();
+    const address = await nft.specificContract.call();
+
+    assert.equal(address, MARKET.address, "'MARKET.address' wasn't in the [specificContract]");
+  });
+});
