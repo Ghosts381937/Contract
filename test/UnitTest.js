ea40c70b9fff80144633dc197da22969045a8771
Ghosts381937 <she.xing88@gmail.com>
2022-07-24 16:54:48 +0800

    Add test for init() and a parser for assertion

--- test/UnitTest.js
+++ test/UnitTest.js
@@ -1,9 +1,21 @@
 const CURRENCY = artifacts.require("Currency");
 const NFT = artifacts.require("Nft");
 const MARKET = artifacts.require("Market");
+const MAJOR = artifacts.require("Major");
+
+const parseContractReturnValue = (returnObject, len) => {
+  for(let i = 0; i < len; i++) {
+    delete returnObject[i];
+  }
+  let objectKeys = Object.keys(returnObject);
+  objectKeys.forEach((key) => {
+    returnObject[key] = returnObject[key].toString();
+  })
+  return returnObject;
+}
 
 contract("CURRENCY", (accounts) => {
-  it("should put 10^22 TFToken in the first account", async () => {
+  it("should put 10^22 TFToken in the first account", async() => {
     const currency = await CURRENCY.deployed();
     const balance = await currency.balanceOf.call(accounts[0]);
 
@@ -11,16 +23,39 @@ contract("CURRENCY", (accounts) => {
   });
 });
 contract("NFT", (accounts) => {
-  it("should be 'Test' in the [name]", async () => {
-    const nft = await NFT.deployed();
+  it("should be 'Test' in the [name]", async() => {
+    const nft = await NFT.deployed()
     const name = await nft.name.call();
-
     assert.equal(name, 'Test', "'Test' wasn't in the [name]");
   });
-  it("should be MARKET.address in the [specificContract]", async () => {
-    const nft = await NFT.deployed();
+  it("should be MARKET.address in the [specificContract]", async() => {
+    const nft = await NFT.deployed()
     const address = await nft.specificContract.call();
-
     assert.equal(address, MARKET.address, "'MARKET.address' wasn't in the [specificContract]");
   });
+});
+contract("MAJOR", (accounts) => {
+  it("The feature of character initialization", async() => {
+    const major = await MAJOR.deployed();
+    let expectedName = "testName";
+    let expectedIsinited = true;
+    let expectedPlayerStatus = {name:expectedName, level:"1", experience: "0", distributableAbility: "0"};
+    let expectedAbility = {str: "10", intllegence: "10", dex: "10", vit: "10", luk: "10"};
+    let expectedEquipment = {helmet: "1", chestplate: "2", leggings: "3", boots: "4", weapon: "10"}
+    await major.init(expectedName, {from:accounts[0]});
+    
+    let actualIsinited = await major.isInited.call(accounts[0]);
+    let actualPlayerStatus = await major.playerStatusOf.call(accounts[0]);
+    actualPlayerStatus = parseContractReturnValue(actualPlayerStatus, Object.keys(expectedPlayerStatus).length);
+    let actualAbility = await major.abilityOf.call(accounts[0]);
+    actualAbility = parseContractReturnValue(actualAbility, Object.keys(expectedAbility).length);
+    let actualEquipment = await major.equipmentOf.call(accounts[0]);
+    actualEquipment = parseContractReturnValue(actualEquipment, Object.keys(expectedEquipment).length);
+
+    
+    assert.deepEqual(expectedIsinited, actualIsinited, "isInited was not successfully alterd");
+    assert.deepEqual(expectedPlayerStatus, actualPlayerStatus, "playerStatus was not successfully alterd");
+    assert.deepEqual(expectedAbility, actualAbility, "ability was not successfully alterd");
+    assert.deepEqual(expectedEquipment, actualEquipment, "equipment was not successfully alterd");
+  })
 });
